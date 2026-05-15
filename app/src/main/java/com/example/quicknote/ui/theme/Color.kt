65e4984258f11e7220e9449bf08badf1b467f42d
mrmusic7654-wq <mr.music7654@gmail.com>
2026-05-15 08:53:14 +0545

    Add app/src/main/java/com/example/quicknote/ui/theme/Color.kt (batch 1/3)

---
+++ app/src/main/java/com/example/quicknote/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.quicknote.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Cyan80 = Color(0xFF80DEEA)
+val CyanGrey80 = Color(0xFFB2EBF2)
+val CyanDark = Color(0xFF006064)
+val BackgroundDark = Color(0xFF121212)
